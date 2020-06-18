3356ff6b890579a6ae053691c5bbbfa09cb56ae3
ThomasSchwaab <thomas@schwaab.bayern>
2020-06-18 18:44:24 +0200

    Add Config for "How much Questions have to be answered until Quiz finishes" (Better for testing)

--- back/includes/config.inc.php
+++ back/includes/config.inc.php
@@ -9,12 +9,16 @@
  * @license    https://directory.fsf.org/wiki/License:X11  MIT/X11/X - Licence
  */
 
+// Set how much Questions have to be answered until finished
+// Keep in Mind, that it starts to Count from 0 ... 39 are already 40
+define("NUM_OF_QUESTIONS", 9);
+
 
 // Enter your Database Data
 $dbhost = 'localhost';
-$dbuser = 'tschwaab_dl';
-$dbpass = 'Of45J3NHBZSI04y5';
-$dbname = 'dl';
+$dbuser = 'c2_quiz';
+$dbpass = '_ojYeeEYh8G';
+$dbname = 'c2_quiz';
 
 $db = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
 // Check connection
